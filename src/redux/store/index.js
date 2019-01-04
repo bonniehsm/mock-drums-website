eb69911e3fef8a1b450c99eb16c8272bc67bac8e
Bonnie <bonnie.hung@hotmail.ca>
2019-01-04 18:24:18 -0500

    removed redux folder

--- src/redux/store/index.js
+++
@@ -1,5 +0,0 @@
-import { createStore } from "redux";
-import rootReducer from "../reducers";
-
-const initialState = { displayable: [0, 1, 2] };
-export const store = createStore(rootReducer, initialState);
